ebd5890716992dd1451012a1078b779f1ecc4d4e
Lukas Materne <lukas_materne@yahoo.de>
2019-10-11 10:51:35 +0200

    test weights and roots

--- test_proj3.py
+++ test_proj3.py
@@ -1,17 +1,27 @@
-from Project3 import GaussLaguerre
-from scipy.special import roots_laguerre
+from Project3 import GaussLaguerre, L_function
+from scipy.special import roots_laguerre, roots_legendre 
 import numpy as np
 import pytest
 
 def test_gausslaguerre():
     """
-    compar own implementation aginst scipy.special,
-    weights in scipy.special are not multiplyed with 2!
+    compar own implementation aginst scipy.special
     """
-    N = 10
+    N = 20
     w, r = GaussLaguerre(N)
     r_e, w_e = roots_laguerre(N)
-    res_w = np.abs(w/2-w_e)
+    res_w = np.abs(w-w_e)
+    res_r  = np.abs(r - r_e)
+    assert np.max(res_w) == pytest.approx(0, abs=10**-5)
+    assert np.max(res_r) == pytest.approx(0)
+def test_gausslegendre():
+    """
+    compar own implementation aginst scipy.special
+    """
+    N = 20
+    w, r = L_function(N)
+    r_e, w_e = roots_legendre(N)
+    res_w = np.abs(w-w_e)
     res_r  = np.abs(r - r_e)
     assert np.max(res_w) == pytest.approx(0)
     assert np.max(res_r) == pytest.approx(0)
