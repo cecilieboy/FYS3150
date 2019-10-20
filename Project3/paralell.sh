78ce0c32adf7ec540f22dafc9427584f057f6182
Lukas Materne <lukas_materne@yahoo.de>
2019-10-20 15:56:49 +0200

    fixed 1e6

--- Project3/paralell.sh
+++ Project3/paralell.sh
@@ -4,7 +4,7 @@ for n in 1 2 4
     count=0
     while [ $count -lt 100 ]
         do 
-        for mc in 100 1000 10000 100000 100000
+        for mc in 100 1000 10000 100000 1000000
             do 
                 mpirun -n $n python P3MPI.py $mc
             done
