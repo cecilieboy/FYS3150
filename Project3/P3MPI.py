73954acfe82b7bd294dccf80a2d34ae951e18974
Lukas Materne <lukas_materne@yahoo.de>
2019-10-20 15:52:09 +0200

    mc parallelization

--- Project3/P3MPI.py
+++ Project3/P3MPI.py
@@ -7,17 +7,18 @@ import time
 from Project3 import integrand_radial
 
 def Random(N):
+
     r1 = np.random.exponential(1,N)
     r2 = np.random.exponential(1,N)
-    ct1 = random.random(N) * 2 - 1
-    ct2 = random.random(N) * 2 - 1
-    p1 = 2 * np.pi * random.random(N)
-    p2 = 2 * np.pi * random.random(N) 
+    ct1, ct2 = 2 * np.random.rand(2 *n).reshape((2,n)) -1
+    p1, p2 = 2 * np.pi * np.random.rand(2 * n).reshape((2,n)) - 1
     return r1, r2, ct1, ct2, p1, p2
 
 
-def integral(n):
-    return 4 * np.pi**2 * np.sum(integrand_radial(*Random(n)))  / 4**5 
+def integral(n):   
+    integral = np.sum(integrand_radial(*Random(n)))
+    return (integral * (4 * np.pi)**2 / 4**5) 
+
 
 comm = MPI.COMM_WORLD
 Size = comm.Get_size()
@@ -28,18 +29,21 @@ n = N // Size
 final_int = 0
 
 start = time.time()
+
+
+
+
+
 final_int += integral(n) / N 
 final_int = comm.reduce(final_int,op = MPI.SUM,root=0)
 
 if rank == 0: 
-    print()
     print('sum:',final_int)
     f = open('time_array','a+')
-    f.write(str(time.time() - start) + '\t' + str(final_int) +  '\t' + str(N) + '\n')
+    f.write(str(Size) + '\t' +str(time.time() - start) + '\t' + str(final_int) +  '\t' + str(N) + '\n')
     f.close()
-    
 
 
 
 
-
+#%%
---
+++ Project3/paralell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for n in 1 2 4
+    do
+    count=0
+    while [ $count -lt 100 ]
+        do 
+        for mc in 100 1000 10000 100000 100000
+            do 
+                mpirun -n $n python P3MPI.py $mc
+            done
+        count=$(( $count + 1 ))
+        done
+    done
+
