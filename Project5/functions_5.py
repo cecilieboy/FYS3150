c41962d248bdb6126aeff2ed64ca650763ac2c4d
Lukas Materne <lukas_materne@yahoo.de>
2019-11-27 21:46:49 +0100

    general runge kutta solver for 1. order ODE systems

--- Project5/functions_5.py
+++ Project5/functions_5.py
@@ -4,18 +4,83 @@ from matplotlib import pyplot as plt
 from tqdm import trange 
 import pandas as pd
 import random 
+import seaborn as sns 
+import matplotlib.pyplot as plt
 #%%
-def g(b, S, I, N=400, a=4, c=0.5):
+def rhs_S(t, S, I, a= 4, b= 1, c= 0.5 , N=400):
     return c * (N - S - I) - a * S * I / N
 
-def h(b, S, I, N=400, a=4, c=0.5):
+def rhs_I(t, S, I, a= 4, b= 1, c= 0.5 , N=400):
+    return a * S * I / N - b * I
+#%%
+def k_updates(RHS, X, RHS_kargs, stepsize):
+    weights = np.array([1,2,2,1])
+    temp = np.copy(X)
+    print(temp)
+    var_len = len(X)-1
+    k = np.zeros(shape=(var_len, 4))
+    for i in range(4):
+        if i > 0:
+            temp[0] = X[0] + stepsize/2
+            temp[1:] = X[1:] + k[:, i-1]/2
+        if i == 3:
+            temp[0] = X[0] + stepsize
+            temp[1:] = X[1:] + k[:, i-1]
+
+        for j in range(var_len):
+            k[j,i] = RHS[j](*temp, **RHS_kargs)
+
+    return weights*k
+
+def runge_kutta(stepsize, t_max, X_0, RHS, kargs_RHS, var_names =["S", "I"]):
+    """
+    flexible runge kutta for 
+    X'(t) = RHS([t, x], **kargs)
+    X is a vector i.e [S,I]
+
+    Args:
+        stepsize for Runge-Kutta
+        t_max for Runge-Kutta
+        X_0 is a list of initial values
+        RHS a list of RHS functions rhs(t,x1,x2,...,x_n, **kargs_rhs),
+        **kargs_RHS a list of keword arguments to each RHS entry
+    Kargs:
+        var_names: list of varibale names
+    """
+    num_steps = int(t_max / stepsize)
+    X_t = np.zeros(shape = (len(X_0) +1, num_steps + 1))
+    X_t[1:, 0]= X_0
+    
+    for i in range (1, num_steps+1):
+        X_t[0, i] = i*stepsize
+        k = k_updates(RHS, X_t[:,i -1 ], kargs_RHS, stepsize)
+        X_t[1:, i] = X_t[1:, i - 1] + stepsize/6*np.sum(k, axis=1)
+ 
+    return pd.DataFrame(X_t.T, columns=np.append(["time"], var_names)) 
+#%%
+df = runge_kutta(0.5, 50, [300, 100], [rhs_S, rhs_I], {} )
+df["R"] = 400- df["S"] - df["I"]
+plt.figure(figsize=(10,10))
+for name in ["S", "I", "R"]:
+    plt.plot(df["time"], df[name], label=name)
+plt.legend(loc='best', fontsize = 28)
+plt.xlabel("Time in a.u.", fontsize = 32)
+plt.ylabel("Number", fontsize = 32)
+plt.tick_params(size =24, labelsize=26)
+plt.tight_layout()
+plt.show()
+
+#%%
+def RHS_S(b, S, I, N=400, a=4, c=0.5):
+    return c * (N - S - I) - a * S * I / N
+
+def RHS_I(b, S, I, N=400, a=4, c=0.5):
     return a * S * I / N - b * I
 
 #def r(b, S, I, N=400, a=4, c=0.5):
 #    return b * I - c * R
 
-#%%
-def runge_katta(stepsize, numb_steps, b = [1,2,3,4], N=400, a=4, c=0.5):
+def runge_kutta(stepsize, numb_steps, b = [1,2,3,4], N=400, a=4, c=0.5):
 
 
     for i in trange(len(b)):
